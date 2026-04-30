390561cdd23157af5a258a9212ffd5818b6508b8
steve-vibecode <stevetest181@gmail.com>
2026-04-30 11:09:01 +0800

    Add files via upload

---
+++ mandarin_assistant/sw.js
@@ -0,0 +1,7 @@
+// Service worker disabled in this clean build to avoid stale GitHub Pages cache.
+self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key)))).then(() => self.clients.claim())
+  );
+});
